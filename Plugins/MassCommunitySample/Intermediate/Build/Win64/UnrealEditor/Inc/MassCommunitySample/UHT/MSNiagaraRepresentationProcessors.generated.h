c78d60af78e0458c9b44bb91a00bb8c71351fab4
Zee-Jay <114673807+Zee-Jay76@users.noreply.github.com>
2025-11-11 19:05:46 +0000

    BACKUP BEFORE PULL

--- Plugins/MassCommunitySample/Intermediate/Build/Win64/UnrealEditor/Inc/MassCommunitySample/UHT/MSNiagaraRepresentationProcessors.generated.h
+++ Plugins/MassCommunitySample/Intermediate/Build/Win64/UnrealEditor/Inc/MassCommunitySample/UHT/MSNiagaraRepresentationProcessors.generated.h
@@ -19,7 +19,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 // ********** Begin Class UMSNiagaraRepresentationProcessors ***************************************
 MASSCOMMUNITYSAMPLE_API UClass* Z_Construct_UClass_UMSNiagaraRepresentationProcessors_NoRegister();
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_INCLASS_NO_PURE_DECLS \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUMSNiagaraRepresentationProcessors(); \
 	friend struct Z_Construct_UClass_UMSNiagaraRepresentationProcessors_Statics; \
@@ -30,7 +30,7 @@ public: \
 	DECLARE_SERIALIZER(UMSNiagaraRepresentationProcessors)
 
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_ENHANCED_CONSTRUCTORS \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_ENHANCED_CONSTRUCTORS \
 	/** Deleted move- and copy-constructors, should never be used */ \
 	UMSNiagaraRepresentationProcessors(UMSNiagaraRepresentationProcessors&&) = delete; \
 	UMSNiagaraRepresentationProcessors(const UMSNiagaraRepresentationProcessors&) = delete; \
@@ -40,12 +40,12 @@ public: \
 	NO_API virtual ~UMSNiagaraRepresentationProcessors();
 
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_11_PROLOG
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_GENERATED_BODY \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_11_PROLOG
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_INCLASS_NO_PURE_DECLS \
-	FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_ENHANCED_CONSTRUCTORS \
+	FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_INCLASS_NO_PURE_DECLS \
+	FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_14_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -57,7 +57,7 @@ class UMSNiagaraRepresentationProcessors;
 // ********** Begin Class UMSNiagaraRepresentationSpawnProcs ***************************************
 MASSCOMMUNITYSAMPLE_API UClass* Z_Construct_UClass_UMSNiagaraRepresentationSpawnProcs_NoRegister();
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_INCLASS_NO_PURE_DECLS \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUMSNiagaraRepresentationSpawnProcs(); \
 	friend struct Z_Construct_UClass_UMSNiagaraRepresentationSpawnProcs_Statics; \
@@ -68,7 +68,7 @@ public: \
 	DECLARE_SERIALIZER(UMSNiagaraRepresentationSpawnProcs)
 
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_ENHANCED_CONSTRUCTORS \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_ENHANCED_CONSTRUCTORS \
 	/** Deleted move- and copy-constructors, should never be used */ \
 	UMSNiagaraRepresentationSpawnProcs(UMSNiagaraRepresentationSpawnProcs&&) = delete; \
 	UMSNiagaraRepresentationSpawnProcs(const UMSNiagaraRepresentationSpawnProcs&) = delete; \
@@ -78,12 +78,12 @@ public: \
 	NO_API virtual ~UMSNiagaraRepresentationSpawnProcs();
 
 
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_28_PROLOG
-#define FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_GENERATED_BODY \
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_28_PROLOG
+#define FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_INCLASS_NO_PURE_DECLS \
-	FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_ENHANCED_CONSTRUCTORS \
+	FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_INCLASS_NO_PURE_DECLS \
+	FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h_31_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -93,6 +93,6 @@ class UMSNiagaraRepresentationSpawnProcs;
 // ********** End Class UMSNiagaraRepresentationSpawnProcs *****************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h
+#define CURRENT_FILE_ID FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_Representation_Processors_MSNiagaraRepresentationProcessors_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Plugins/MassCommunitySample/Intermediate/Build/Win64/UnrealEditor/Inc/MassCommunitySample/UHT/MSProjectileHitObservers.gen.cpp
+++ Plugins/MassCommunitySample/Intermediate/Build/Win64/UnrealEditor/Inc/MassCommunitySample/UHT/MSProjectileHitObservers.gen.cpp
@@ -183,15 +183,15 @@ UMSEntityWasHitSignalProcessor::~UMSEntityWasHitSignalProcessor() {}
 // ********** End Class UMSEntityWasHitSignalProcessor *********************************************
 
 // ********** Begin Registration *******************************************************************
-struct Z_CompiledInDeferFile_FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics
+struct Z_CompiledInDeferFile_FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
 		{ Z_Construct_UClass_UMSProjectileHitObservers, UMSProjectileHitObservers::StaticClass, TEXT("UMSProjectileHitObservers"), &Z_Registration_Info_UClass_UMSProjectileHitObservers, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMSProjectileHitObservers), 1380000170U) },
 		{ Z_Construct_UClass_UMSEntityWasHitSignalProcessor, UMSEntityWasHitSignalProcessor::StaticClass, TEXT("UMSEntityWasHitSignalProcessor"), &Z_Registration_Info_UClass_UMSEntityWasHitSignalProcessor, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UMSEntityWasHitSignalProcessor), 305413274U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_2741127843(TEXT("/Script/MassCommunitySample"),
-	Z_CompiledInDeferFile_FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_joshm_Desktop_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics::ClassInfo),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_2741127843(TEXT("/Script/MassCommunitySample"),
+	Z_CompiledInDeferFile_FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_SOL_DRIFT_P4_Plugins_MassCommunitySample_Source_MassCommunitySample_ProjectileSim_Processors_MSProjectileHitObservers_h__Script_MassCommunitySample_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
 // ********** End Registration *********************************************************************
