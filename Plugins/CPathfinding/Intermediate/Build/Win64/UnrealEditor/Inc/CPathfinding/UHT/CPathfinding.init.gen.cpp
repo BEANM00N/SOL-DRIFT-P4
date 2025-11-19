ed2e82d1b9168b91da26a6cdd47b8e0947011cc8
BEANM00N <joshmccamley@gmail.com>
2025-11-19 17:44:44 +0000

    Mass Entities hit enemies!

--- Plugins/CPathfinding/Intermediate/Build/Win64/UnrealEditor/Inc/CPathfinding/UHT/CPathfinding.init.gen.cpp
+++ Plugins/CPathfinding/Intermediate/Build/Win64/UnrealEditor/Inc/CPathfinding/UHT/CPathfinding.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeCPathfinding_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x4A4C37B6,
+				0xA11F7C39,
 				0xEFD1B2E6,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeCPathfinding_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_CPathfinding.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CPathfinding(Z_Construct_UPackage__Script_CPathfinding, TEXT("/Script/CPathfinding"), Z_Registration_Info_UPackage__Script_CPathfinding, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4A4C37B6, 0xEFD1B2E6));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CPathfinding(Z_Construct_UPackage__Script_CPathfinding, TEXT("/Script/CPathfinding"), Z_Registration_Info_UPackage__Script_CPathfinding, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA11F7C39, 0xEFD1B2E6));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
