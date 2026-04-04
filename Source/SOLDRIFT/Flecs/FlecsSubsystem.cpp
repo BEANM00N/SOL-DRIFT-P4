23a2a14a2f8ace0cfa127a6f360968e6b7115aaa
BEANM00N <joshmccamley@gmail.com>
2026-04-04 01:29:44 +0100

    Utility Fixup

--- Source/SOLDRIFT/Flecs/FlecsSubsystem.cpp
+++ Source/SOLDRIFT/Flecs/FlecsSubsystem.cpp
@@ -3,129 +3,153 @@
 
 void UFlecsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-	Super::Initialize(Collection);
+    Super::Initialize(Collection);
 
-	EcsWorld = new flecs::world();
+    EcsWorld = new flecs::world();
 
-	if (UWorld* World = GetWorld())
-	{
-        // FIX: Binding to our updated TickSimulation function
-		TickDelegateHandle = FWorldDelegates::OnWorldTickStart.AddUObject(this, &UFlecsSubsystem::TickSimulation);
-	}
+    if (UWorld* World = GetWorld())
+    {
+       // Bind Physics to start of frame
+       TickDelegateHandle = FWorldDelegates::OnWorldTickStart.AddUObject(this, &UFlecsSubsystem::TickSimulation);
+       // Bind Rendering to end of frame (Post-Actor tick)
+       PresentationDelegateHandle = FWorldDelegates::OnWorldPostActorTick.AddUObject(this, &UFlecsSubsystem::UpdatePresentation);
+    }
 
-	// ---------------------------------------------------------
-	// SYSTEM 1: MOVEMENT
-	// FIX: Using FLECS v4 .each() 
-	// ---------------------------------------------------------
-	EcsWorld->system<Position, const Velocity>("Movement")
-		.each([](flecs::iter& it, size_t i, Position& p, const Velocity& v) 
-		{
-			p.Value += v.Value * it.delta_time();
-		});
+    // ---------------------------------------------------------
+    // SYSTEM 1: MOVEMENT
+    // ---------------------------------------------------------
+    EcsWorld->system<Position, const Velocity>("Movement")
+       .each([](flecs::iter& it, size_t i, Position& p, const Velocity& v) 
+       {
+          p.Value += v.Value * it.delta_time();
+       });
 
-	// ---------------------------------------------------------
-	// SYSTEM 2: RAYCAST COLLISION
-	// ---------------------------------------------------------
-	UWorld* WorldPtr = GetWorld();
-	EcsWorld->system<Position, const Velocity>("Collision")
-		.each([WorldPtr](flecs::iter& it, size_t i, Position& p, const Velocity& v) 
-		{
-			if (!WorldPtr) return;
-			float dt = it.delta_time();
+    // ---------------------------------------------------------
+    // SYSTEM 2: RAYCAST COLLISION
+    // ---------------------------------------------------------
+    UWorld* WorldPtr = GetWorld();
+    EcsWorld->system<Position, const Velocity>("Collision")
+       .each([WorldPtr](flecs::iter& it, size_t i, Position& p, const Velocity& v) 
+       {
+          if (!WorldPtr) return;
+          float dt = it.delta_time();
 
-			FVector Start = p.Value - (v.Value * dt); 
-			FVector End = p.Value;
+          FVector Start = p.Value - (v.Value * dt); 
+          FVector End = p.Value;
 
-			FHitResult Hit;
-			FCollisionQueryParams Params;
+          FHitResult Hit;
+          FCollisionQueryParams Params;
 
-			if (WorldPtr->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params))
-			{
-				p.Value = Hit.ImpactPoint;
-				it.entity(i).set<HitEvent>({ Hit });
-			}
-		});
+          if (WorldPtr->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params))
+          {
+             p.Value = Hit.ImpactPoint;
+             it.entity(i).set<HitEvent>({ Hit });
+          }
+       });
 
-	// ---------------------------------------------------------
-	// SYSTEM 3: PROCESS HITS
-	// ---------------------------------------------------------
-	EcsWorld->system<const HitEvent, const DamagePayload, const Affiliation>("ProcessHits")
-		.each([](flecs::iter& it, size_t i, const HitEvent& hitEvent, const DamagePayload& damage, const Affiliation& affil) 
-		{
-			flecs::entity e = it.entity(i);
-			AActor* HitActor = hitEvent.Hit.GetActor();
+    // ---------------------------------------------------------
+    // SYSTEM 3: PROCESS HITS
+    // ---------------------------------------------------------
+    EcsWorld->system<const HitEvent, const DamagePayload, const Affiliation>("ProcessHits")
+       .each([](flecs::iter& it, size_t i, const HitEvent& hitEvent, const DamagePayload& damage, const Affiliation& affil) 
+       {
+          flecs::entity e = it.entity(i);
+          AActor* HitActor = hitEvent.Hit.GetActor();
 
-			if (HitActor) 
-			{
-				UGameplayStatics::ApplyDamage(HitActor, damage.Amount, nullptr, nullptr, UDamageType::StaticClass());
-			}
+          if (HitActor) 
+          {
+             UGameplayStatics::ApplyDamage(HitActor, damage.Amount, nullptr, nullptr, UDamageType::StaticClass());
+          }
 
-			if (e.has<CanPenetrate>()) {
-				e.remove<HitEvent>();
-			} else if (e.has<CanRicochet>()) {
-				e.remove<HitEvent>();
-			} else {
-				e.destruct();
-			}
-		});
+          if (e.has<CanPenetrate>()) {
+             e.remove<HitEvent>();
+          } else if (e.has<CanRicochet>()) {
+             e.remove<HitEvent>();
+          } else {
+             e.destruct();
+          }
+       });
 
-	// ---------------------------------------------------------
-	// SYSTEM 4: GATHER NIAGARA DATA
-	// ---------------------------------------------------------
-	EcsWorld->system<const Position, const Velocity>("UpdateNiagara")
-		.kind(flecs::PostUpdate) 
-		.each([this](flecs::iter& it, size_t i, const Position& p, const Velocity& v) 
-		{
-			if (!ProjectileNiagaraComponent) return;
-
-            // We just add to the arrays here. Resetting and Pushing happens in TickSimulation!
-			NiagaraPositions.Add(p.Value);
-			NiagaraRotations.Add((FQuat4f)v.Value.ToOrientationQuat());
-			NiagaraIDs.Add(static_cast<int32>(it.entity(i).id()));
-		});
+    // ---------------------------------------------------------
+    // INIT RENDER QUERY
+    // ---------------------------------------------------------
+    // FIX: We allocate the query to our pointer so it's safely tracked
+    RenderQuery = new flecs::query<const Position, const Velocity, const RibbonID>(
+        EcsWorld->query<const Position, const Velocity, const RibbonID>()
+    );
 }
 
 void UFlecsSubsystem::Deinitialize()
 {
-	FWorldDelegates::OnWorldTickStart.Remove(TickDelegateHandle);
-	if (EcsWorld) { delete EcsWorld; EcsWorld = nullptr; }
-	Super::Deinitialize();
+    FWorldDelegates::OnWorldTickStart.Remove(TickDelegateHandle);
+    FWorldDelegates::OnWorldPostActorTick.Remove(PresentationDelegateHandle);
+    
+    // FIX: Explicitly delete the query before the ECS World dies!
+    if (RenderQuery) 
+    { 
+        delete RenderQuery; 
+        RenderQuery = nullptr; 
+    }
+
+    if (EcsWorld) 
+    { 
+        delete EcsWorld; 
+        EcsWorld = nullptr; 
+    }
+    
+    Super::Deinitialize();
 }
 
-// FIX: Signature updated to match Unreal, Array pushes moved here
+// ---------------------------------------------------------
+// TICK SIMULATION (Runs before Blueprints / Movement happens here)
+// ---------------------------------------------------------
 void UFlecsSubsystem::TickSimulation(UWorld* World, ELevelTick TickType, float DeltaTime)
 {
-	if (EcsWorld) 
-	{ 
-        // 1. Reset arrays ONCE per frame
-		NiagaraPositions.Reset();
-		NiagaraRotations.Reset();
-		NiagaraIDs.Reset();
+    if (EcsWorld) 
+    { 
+       EcsWorld->progress(DeltaTime); 
+    }
+}
 
-        // 2. Run the ECS simulation (This populates the arrays)
-		EcsWorld->progress(DeltaTime); 
+// ---------------------------------------------------------
+// UPDATE PRESENTATION (Runs after Blueprints / Rendering happens here)
+// ---------------------------------------------------------
+void UFlecsSubsystem::UpdatePresentation(UWorld* World, ELevelTick TickType, float DeltaTime)
+{
+	if (!EcsWorld || !ProjectileNiagaraComponent || !RenderQuery) return;
 
-        // 3. Push to Niagara ONCE per frame
-		if (ProjectileNiagaraComponent && NiagaraPositions.Num() > 0)
-		{
-			UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector(ProjectileNiagaraComponent, FName("MassParticlePositions"), NiagaraPositions);
-			UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayQuat(ProjectileNiagaraComponent, FName("MassParticleOrientations"), NiagaraRotations);
-			UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayInt32(ProjectileNiagaraComponent, FName("MassParticleIDs"), NiagaraIDs);
-		}
-	}
+	NiagaraPositions.Reset();
+	NiagaraRotations.Reset();
+	NiagaraIDs.Reset();
+
+	RenderQuery->each([this](flecs::iter& it, size_t i, const Position& p, const Velocity& v, const RibbonID& ribbon) 
+	{
+		NiagaraPositions.Add(p.Value);
+		NiagaraRotations.Add((FQuat4f)v.Value.ToOrientationQuat());
+		NiagaraIDs.Add(ribbon.Value);
+	});
+
+	// FIX: Removed the `if > 0` check. 
+	// We MUST push the empty arrays so Niagara knows to clear the screen!
+	UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector(ProjectileNiagaraComponent, FName("MassParticlePositions"), NiagaraPositions);
+	UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayQuat(ProjectileNiagaraComponent, FName("MassParticleOrientations"), NiagaraRotations);
+	UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayInt32(ProjectileNiagaraComponent, FName("MassParticleIDs"), NiagaraIDs);
 }
 
 void UFlecsSubsystem::SpawnFlecsProjectile(FVector SpawnLocation, FVector StartVelocity, float Damage, ETeamAffiliation Team, bool bPenetrates, bool bRicochets)
 {
-	if (!EcsWorld) return;
+    if (!EcsWorld) return;
 
-	flecs::entity e = EcsWorld->entity()
-		.set<Position>({ SpawnLocation })
-		.set<Velocity>({ StartVelocity })
-		.set<DamagePayload>({ Damage })
-		.set<Affiliation>({ Team })
-		.add<ProjectileTag>();
+    int32 RibbonIDVal = CurrentRibbonID++;
 
-	if (bPenetrates) e.add<CanPenetrate>();
-	if (bRicochets) e.add<CanRicochet>();
+    flecs::entity e = EcsWorld->entity()
+       .set<Position>({ SpawnLocation })
+       .set<Velocity>({ StartVelocity })
+       .set<DamagePayload>({ Damage })
+       .set<Affiliation>({ Team })
+       .set<RibbonID>({ RibbonIDVal })
+       .add<ProjectileTag>();
+
+    if (bPenetrates) e.add<CanPenetrate>();
+    if (bRicochets) e.add<CanRicochet>();
 }
--- Source/SOLDRIFT/Flecs/FlecsSubsystem.h
+++ Source/SOLDRIFT/Flecs/FlecsSubsystem.h
@@ -6,8 +6,6 @@
 #include "NiagaraDataInterfaceArrayFunctionLibrary.h"
 
 THIRD_PARTY_INCLUDES_START
-
-// This specifically tells the Windows compiler to ignore the "Unreachable Code" warning
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable: 4702) 
@@ -18,7 +16,6 @@ THIRD_PARTY_INCLUDES_START
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
-
 THIRD_PARTY_INCLUDES_END
 
 #include "FlecsSubsystem.generated.h"
@@ -27,6 +24,7 @@ THIRD_PARTY_INCLUDES_END
 struct Position { FVector Value; };
 struct Velocity { FVector Value; };
 struct DamagePayload { float Amount; };
+struct RibbonID { int32 Value; };
 
 // Tags
 struct ProjectileTag {};
@@ -37,38 +35,47 @@ struct HitEvent { FHitResult Hit; };
 UENUM(BlueprintType)
 enum class ETeamAffiliation : uint8
 {
-	Friendly,
-	Enemy,
-	Neutral
+    Friendly,
+    Enemy,
+    Neutral
 };
 struct Affiliation { ETeamAffiliation Team; };
 
 UCLASS()
 class SOLDRIFT_API UFlecsSubsystem : public UGameInstanceSubsystem
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	virtual void Deinitialize() override;
-	
-	// FIX: Updated signature to match Unreal's OnWorldTickStart
-	void TickSimulation(UWorld* World, ELevelTick TickType, float DeltaTime);
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+    
+    // Run ECS Math at the start of the frame
+    void TickSimulation(UWorld* World, ELevelTick TickType, float DeltaTime);
+    
+    // Gather Niagara data at the end of the frame (0 latency)
+    void UpdatePresentation(UWorld* World, ELevelTick TickType, float DeltaTime);
 
-	UFUNCTION(BlueprintCallable, Category = "FLECS|Projectiles")
-	void SpawnFlecsProjectile(FVector SpawnLocation, FVector StartVelocity, float Damage, ETeamAffiliation Team, bool bPenetrates, bool bRicochets);
+    UFUNCTION(BlueprintCallable, Category = "FLECS|Projectiles")
+    void SpawnFlecsProjectile(FVector SpawnLocation, FVector StartVelocity, float Damage, ETeamAffiliation Team, bool bPenetrates, bool bRicochets);
 
-	UPROPERTY(BlueprintReadWrite, Category = "FLECS|Rendering")
-	UNiagaraComponent* ProjectileNiagaraComponent;
+    UPROPERTY(BlueprintReadWrite, Category = "FLECS|Rendering")
+    UNiagaraComponent* ProjectileNiagaraComponent;
 
 private:
-	flecs::world* EcsWorld = nullptr;
-	FDelegateHandle TickDelegateHandle;
+    flecs::world* EcsWorld = nullptr;
+    FDelegateHandle TickDelegateHandle;
+    FDelegateHandle PresentationDelegateHandle;
 
-	UPROPERTY()
-	TArray<FVector> NiagaraPositions;
-	UPROPERTY()
-	TArray<FQuat4f> NiagaraRotations;
-	UPROPERTY()
-	TArray<int32> NiagaraIDs;
+    // FIX: This is now a pointer so we can safely delete it before EcsWorld is destroyed
+    flecs::query<const Position, const Velocity, const RibbonID>* RenderQuery = nullptr;
+
+    UPROPERTY()
+    TArray<FVector> NiagaraPositions;
+    UPROPERTY()
+    TArray<FQuat4f> NiagaraRotations;
+    UPROPERTY()
+    TArray<int32> NiagaraIDs;
+    
+    int32 CurrentRibbonID = 1;
 };
